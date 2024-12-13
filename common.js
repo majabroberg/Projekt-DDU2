b5453c21181a5fd06c28791e4702c29d79dea3c4
majabroberg <maja.broberg@outlook.com>
2024-12-13 14:49:33 +0100

    Fixade home länk med JS
    Har skapat en länk med JS så man kan komma tillbaka till home sidan. Tog bort den ur HTML då den inte behövdes där.

---
+++ common.js
@@ -0,0 +1,6 @@
+const links = document.querySelector("#links")
+const home = document.createElement("a");
+links.appendChild(home);
+home.textContent = "Home";
+home.setAttribute("href", "../index.html")
+
