a7485554d831ec218a5e12abcd0670076f3b8bfe
maja <maja.broberg@outlook.com>
2025-01-06 16:21:15 +0100

    JS och CSS för skapandet av celler
    Kodat så man kan trycka på knappen create och det skapas så många celler som man skrivit in i rutan samt att det startar med 95 celler redan inlagda. Även lagt till lite CSS.

--- common.js
+++ common.js
@@ -20,11 +20,43 @@ button.textContent = "Create"
 
 
 function randomNumber() {
-    number = Math.ceil(100 * Math.random());
+    number = Math.ceil(99 * Math.random());
     return number;
 }
 
+input.value = "95"
+function startGrid() {
+    let grid = document.createElement("div");
+    grid.setAttribute("id", "grid"); document.body.appendChild(grid);
+    for (let i = 0; i < 95; i++) {
+        let cell = document.createElement("div")
+        grid.appendChild(cell)
+        cell.setAttribute("class", "cell");
+        cell.textContent = randomNumber();
+    }
+}
+startGrid();
 
+button.addEventListener("click", function () {
+    const inputValue = input.value;
+    let grid = document.getElementById("grid");
+
+    if (grid) {
+        grid.innerHTML = '';
+    }
+    else {
+        let grid = document.createElement("div");
+        body.insertBefore(grid, script);
+        grid.setAttribute("id", "grid");
+    }
+
+    for (let i = 0; i < inputValue; i++) {
+        let cell = document.createElement("div")
+        grid.appendChild(cell)
+        cell.setAttribute("class", "cell");
+        cell.textContent = randomNumber();
+    }
+})
 
 
 
