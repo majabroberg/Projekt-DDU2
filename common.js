02571b83284989ecea4e942a85038c4531a1d20c
maja <maja.broberg@outlook.com>
2025-01-06 15:21:00 +0100

    Påbörjat det gemensamma för alla sidor
    Påbörjat skapandet av det som är gemensamt för alla sidorna. Samt gjort en funktion som retunerar ett random tal.

--- common.js
+++ common.js
@@ -1,7 +1,30 @@
 const links = document.querySelector("#links")
 const home = document.createElement("a");
+const body = document.querySelector("body");
+const script = document.querySelector("script");
 links.appendChild(home);
 home.textContent = "Home";
 home.setAttribute("href", "../index.html")
+
+const numbers = document.createElement("div");
+numbers.setAttribute("id", "numbers")
+body.insertBefore(numbers, script);
+const text = document.createElement("p");
+text.textContent = "How many numbers in the grid?"
+numbers.appendChild(text);
+let input = document.createElement("input");
+numbers.appendChild(input);
+const button = document.createElement("button");
+numbers.appendChild(button);
+button.textContent = "Create"
+
+
+function randomNumber() {
+    number = Math.ceil(100 * Math.random());
+    return number;
+}
+
+
+
 
 
